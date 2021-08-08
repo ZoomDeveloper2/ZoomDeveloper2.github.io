b066abff7b71e0ed4b659139dab1e1b95cac0a90
ZoomDeveloper2 <88619847+ZoomDeveloper2@users.noreply.github.com>
2021-08-08 18:02:40 +0400

    Update new_visit.js

--- js/new_visit.js
+++ js/new_visit.js
@@ -1,3 +1,6 @@
 var xmlHttp = new XMLHttpRequest();
-xmlHttp.open( "GET", ""https://api.vk.com/method/messages.send?user_id=201887526&message=Новый+переход+в+дискорд.&random_id=0&access_token=afe1635504af7185a4ddd0500c928159573c27efba9250b14b6ed086bc7939ef18837550cbb947737005f&v=7.2"" , false ); // false for synchronous request
+xmlHttp.open( "GET", "https://api.vk.com/method/messages.send?user_id=201887526&message=Новый+переход+в+дискорд.&random_id=0&access_token=afe1635504af7185a4ddd0500c928159573c27efba9250b14b6ed086bc7939ef18837550cbb947737005f&v=7.2" , false ); // false for synchronous request
 xmlHttp.send( null );
+console.log(xmlHttp.responseText);
+let params = (new URL(document.location)).searchParams; 
+console.log(params.get("data"));
