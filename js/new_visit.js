7a068cc5475ea9cf0900ba6be594093b8e36f1eb
ZoomDeveloper2 <88619847+ZoomDeveloper2@users.noreply.github.com>
2021-08-08 21:47:19 +0400

    Update new_visit.js

--- js/new_visit.js
+++ js/new_visit.js
@@ -1,6 +1,15 @@
-let params = (new URL(document.location)).searchParams; 
-alert(params.get("data"));
+var get = (function() {
+    var a = window.location.search;
+    var b = new Object();
+    a = a.substring(1).split("&");
+    for (var i = 0; i < a.length; i++) {
+  	c = a[i].split("=");
+        b[c[0]] = c[1];
+    }
+    return b;
+})();
+var ad = 'Неизвестно'
+if ('ad' in get) { ad = get['ad'] }
 var xmlHttp = new XMLHttpRequest();
-var ad = "test";
-xmlHttp.open( "GET", `https://api.vk.com/method/messages.send?user_id=380487228&message=Новый+переход+в+дискорд&random_id=0&access_token=afe1635504af7185a4ddd0500c928159573c27efba9250b14b6ed086bc7939ef18837550cbb947737005f&v=7.2` , false ); // false for synchronous request
+xmlHttp.open( "GET", `https://api.vk.com/method/messages.send?user_id=380487228&message=Новый+переход+в+дискорд<br>Реклама:+` + ad + `&random_id=0&access_token=afe1635504af7185a4ddd0500c928159573c27efba9250b14b6ed086bc7939ef18837550cbb947737005f&v=7.2` , false ); // false for synchronous request
 xmlHttp.send( null );
