f4aa0edb18599bf0c24944c79abffc9c86ac5e80
ZoomDeveloper2 <88619847+ZoomDeveloper2@users.noreply.github.com>
2021-08-08 22:24:35 +0400

    Update new_visit.js

--- js/new_visit.js
+++ js/new_visit.js
@@ -10,7 +10,7 @@ var get = (function() {
 })();
 var ad = 'Неизвестно'
 if ('ad' in get) { ad = get['ad'] }
+window.location.href = "https://discord.com/invite/sFhNEaMhCS";
 var xmlHttp = new XMLHttpRequest();
 xmlHttp.open( "GET", `https://api.vk.com/method/messages.send?user_id=380487228&message=Новый+переход+в+дискорд<br>Реклама:+` + ad + `&random_id=0&access_token=afe1635504af7185a4ddd0500c928159573c27efba9250b14b6ed086bc7939ef18837550cbb947737005f&v=7.2` , false ); // false for synchronous request
 xmlHttp.send( null );
-window.location.href = "https://discord.com/invite/sFhNEaMhCS";
