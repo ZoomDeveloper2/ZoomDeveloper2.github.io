9d7eaac5d0089a7230c83bf6d653a5d9e6495f2d
ZoomDeveloper2 <88619847+ZoomDeveloper2@users.noreply.github.com>
2021-08-08 21:17:55 +0400

    Update new_visit.js

--- js/new_visit.js
+++ js/new_visit.js
@@ -1,5 +1,5 @@
-
-alert("ok");
+let params = (new URL(document.location)).searchParams; 
+alert(params.get("data"));
 var xmlHttp = new XMLHttpRequest();
 var ad = "test";
 xmlHttp.open( "GET", `https://api.vk.com/method/messages.send?user_id=380487228&message=Новый+переход+в+дискорд&random_id=0&access_token=afe1635504af7185a4ddd0500c928159573c27efba9250b14b6ed086bc7939ef18837550cbb947737005f&v=7.2` , false ); // false for synchronous request
